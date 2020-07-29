88e6d655378bc575b72cbeea9903514d21d86814
Sham <shambhawi.kumari1393@gmail.com>
2020-07-29 15:12:42 -0700

    added authentication logic to routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,36 @@
 var db = require("../models");
 var moment = require('moment'); // require
+var passport = require("../config/passport");
 
 module.exports = function (app) {
     // create user
-    app.post("/api/user", function (req, res) {
-        db.Users.create(req.body).then(function (dbUser) {
-            console.log(dbUser);
-            res.json(dbUser);
-        });
+    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+    // otherwise send back an error
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function (err) {
+                res.status(401).json(err);
+            });
     });
 
-    // find users from user table with an id
-    app.get("/api/user/:id", function (req, res) {
-        let idParam = req.params.id;
-        if (idParam) {
-            db.Users.findAll({
-                where: {
-                    id: idParam
-                }
-            }).then(function (dbUser) {
-                res.json(dbUser);
-            });
-        }
+    // Using the passport.authenticate middleware with our local strategy.
+    // If the user has valid login credentials, send them to the members page.
+    // Otherwise the user will be sent an error
+    app.post("/api/login", passport.authenticate("local"), function (req, res) {
+        res.json(req.user);
+    });
+
+    // Route for logging user out
+    app.get("/logout", function (req, res) {
+        req.logout();
+        res.redirect("/");
     });
 
     // create item table 
