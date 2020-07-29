e7a17a1c15e83e9d9bbec3a102af2ce545159407
Sham <shambhawi.kumari1393@gmail.com>
2020-07-29 09:23:53 -0700

    html routes skeleton

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,31 +8,39 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads login/signup page
-  app.get("/", function(req, res) {
-      //test
-    //res.sendFile(path.join(__dirname, "../public/test.html"));
-    
-    //res.render("index", hbsObject);
-  });
+    // index route loads login/signup page
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/test.html"));
+        //res.render("index", hbsObject);
+    });
 
-  // loads dashboard page withlist of items available in refrigerator
-  app.get("/dashboard", function(req, res) {
-    
-  });
+    // loads dashboard page withlist of items available in refrigerator
+    app.get("/dashboard", function (req, res) {
 
-  // shows list of alerts
-  app.get("/notification", function(req, res) {
-    
-  });
+    });
 
-  // shows list of user saves recipes
-  app.get("/recipes", function(req, res) {
-    
-  });
+    // shows list of items in fridge
+    app.get("/fridge/view", function (req, res) {
+
+    });
+
+    // edit list of items in fridge
+    app.get("/fridge/edit", function (req, res) {
+
+    });
+
+    // add items in fridge
+    app.get("/fridge/addItem", function (req, res) {
+
+    });
+
+    // alert user 
+    app.get("/alert", function (req, res) {
+
+    });
 
 };
