7aa338f9001c4152bb5632928ecd97c191ec9a35
Sham <shambhawi.kumari1393@gmail.com>
2020-07-29 09:57:57 -0700

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,33 +14,33 @@ module.exports = function (app) {
 
     // index route loads login/signup page
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/test.html"));
+        res.sendFile(path.join(__dirname, "../public/login.html"));
         //res.render("index", hbsObject);
     });
 
     // loads dashboard page withlist of items available in refrigerator
     app.get("/dashboard", function (req, res) {
-
+        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
     });
 
     // shows list of items in fridge
     app.get("/fridge/view", function (req, res) {
-
+        res.sendFile(path.join(__dirname, "../public/view-fridge.html"));
     });
 
     // edit list of items in fridge
     app.get("/fridge/edit", function (req, res) {
-
+        res.sendFile(path.join(__dirname, "../public/edit-fridge.html"));
     });
 
     // add items in fridge
     app.get("/fridge/addItem", function (req, res) {
-
+        res.sendFile(path.join(__dirname, "../public/Add-new-item.html"));
     });
 
     // alert user 
     app.get("/alert", function (req, res) {
-
+        res.sendFile(path.join(__dirname, "../public/alert.html"));
     });
 
 };
